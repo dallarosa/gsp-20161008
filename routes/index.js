fa01310f749b08431dd92c5a27f6a19ad6ec8e64
Francisco Dalla Rosa Soares <dallarosa@gmail.com>
2016-10-08 12:15:06 +0900

    added webhook

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/webhook',function(req,res) {
+  if(req.query['hub.verify_token'] === "my_whatever_verification_token") {
+    res.send(res.query[hub.challenge]);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
 
 module.exports = router;
