cc8eac5c6972d3f0cf9804c26c8e34aaba95d86e
Francisco Dalla Rosa Soares <dallarosa@gmail.com>
2016-10-08 13:14:35 +0900

    added send message

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 router.get('/webhook',function(req,res) {
   if(req.query['hub.verify_token'] === "my_whatever_verification_token") {
     res.send(req.query['hub.challenge']);
@@ -12,5 +13,41 @@ router.get('/webhook',function(req,res) {
     res.send('Error, wrong validation token');
   }
 });
+
+router.post('/webhook', function(req,res) {
+  const events = req.body.entry[0].messaging;
+  for(i = 0; i < events.length; i++) {
+    const event = req.body.entry[0].messaging[i];
+    const sender = event.sender.id;
+    if(event.message && event.message.text) {
+      const text = event.message.text;
+      console.log(text);
+      sendTextMessage(sender, "Text received, echo: " + text);
+    }
+  }
+  res.sendStatus(200);
+});
+
+var request = require('request');
+
+const ACCESS_TOKEN = process.env.FB_ACCESS_TOKEN;
+
+function sendTextMessage(sender, text) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function(error, response,body) {
+    if(error) {
+      console.log('Error sending message: ', error);
+    } else if(response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 module.exports = router;
